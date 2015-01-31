1ed9152e9f07f62b8eb2e372a89702dde856050d
Preld <programmer.called.azathoth@gmail.com>
2015-02-01 06:50:30 +0900

    Add GraphObject

---
+++ Assets/Oscilloscope/Scripts/Graph.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Graph : MonoBehaviour
+{
+
+		private const int RESOLUTION_WIDTH = 240;
+	
+		private LineRenderer _lineRenderer;
+		private float _backgroundHeight;
+		private float _backgroundWidth;
+		private Vector3 _centerLineBegin;
+		private Vector3 _centerLineEnd;
+		private Bounds _myBounds;
+
+		private Vector3 _baseLinePos;
+		private Vector3 _baseX;
+		private Vector3 _baseY;
+		private List<float> _yPoints;
+
+		void Start ()
+		{
+				// init line
+				_lineRenderer = gameObject.AddComponent (typeof(LineRenderer)) as LineRenderer;
+				_lineRenderer.material.color = new Color (0, 0, 0, 1.0f);
+				_lineRenderer.SetWidth (0.05f, 0.05f);
+				_lineRenderer.SetVertexCount (RESOLUTION_WIDTH);	
+
+				// init positions	
+				_myBounds = GetBound ();
+				Vector3 objectSize = new Vector3 (
+						                     _myBounds.extents.x * this.transform.lossyScale.x,
+						                     _myBounds.extents.y * this.transform.lossyScale.y,
+						                     _myBounds.extents.z * this.transform.lossyScale.z
+				                     );
+				_baseLinePos = new Vector3 (
+						transform.position.x - objectSize.x,
+						transform.position.y - objectSize.z,
+						transform.position.z - 0.1f //カメラの方へ少しずらす
+				);
+				/*_centerLineBegin = new Vector3 (
+						transform.position.x - objectSize.x,
+						transform.position.y,
+						transform.position.z - 0.1f //カメラの方へ少しずらす
+				);
+				_centerLineEnd = new Vector3 (
+						transform.position.x + objectSize.x,
+						transform.position.y,
+						transform.position.z - 0.1f //カメラの方へ少しずらす
+				);*/
+
+				// init size
+				_backgroundHeight = objectSize.z * 2f; // 回転しているため
+				_backgroundWidth = objectSize.x * 2f;
+				_baseX = new Vector3 (
+						_backgroundWidth / (RESOLUTION_WIDTH - 1),
+						0,
+						0
+				);
+				_baseY = new Vector3 (
+						0,
+						_backgroundHeight,
+						0
+				);
+						
+				// dummy data
+				_yPoints = new List<float> ();
+				for (int i = 0; i < RESOLUTION_WIDTH; i++) {
+						_yPoints.Add (0f);
+				}
+		}
+
+		void Update ()
+		{
+				for (int i = 0; i < RESOLUTION_WIDTH; i++) {
+						_lineRenderer.SetPosition (i, _baseLinePos + _baseX * i + _baseY * _yPoints [i]);
+				}
+		}
+
+		public void setValue (float value)
+		{
+				_yPoints.Add (value);
+				_yPoints.RemoveAt (0);
+		}
+
+		private Bounds GetBound ()
+		{
+				Bounds bounds = this.GetComponent<MeshFilter> ().mesh.bounds;
+				//bounds.center = Vector3.zero;
+				return bounds;
+		}
+}
---
+++ Assets/Oscilloscope/Scripts/GraphMaster.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphMaster : MonoBehaviour
+{
+		private ConnectInterface _devices;
+		private Graph[] _graphs;
+
+		private float beforeValue;
+
+		void Start ()
+		{
+				_devices = GameObject.FindGameObjectWithTag ("Arduino").GetComponent<Connect2Arduino> ();
+				_devices.setPort ("COM3", 115200, 1);
+				//("/dev/tty.usbmodem14121", 115200);
+				//("/dev/tty.usbmodem1451", 115200);
+				//("COM3", 115200);
+				GameObject[] gameObjects = GameObject.FindGameObjectsWithTag ("Graph");
+				_graphs = new Graph[gameObjects.Length];
+				for (int i = 0; i < _graphs.Length; i++) {
+						_graphs [i] = gameObjects [i].GetComponent<Graph> ();
+				}
+		}
+
+		void Update ()
+		{
+				//float[] data = _devices.getVoltages ();
+				float[] data = new float[2];
+				data [0] = Mathf.Abs (Mathf.Sin (Time.time * 3f));
+				data [1] = Mathf.Abs (Mathf.Sin (Time.time * 5f));
+
+				// processing data
+				//_graphs [0].setValue (data [0] / 1023f);
+				//float tmp = (data [0] * 0.1f + before_data * 0.9f);
+				//m_wave [1].addData (tmp / 1023f);
+				//before_data = tmp;
+
+				// set value
+				_graphs [0].setValue (data [0]);
+				_graphs [1].setValue (data [1]);
+		}
+
+		public void fitButton ()
+		{
+		}
+				
+}
